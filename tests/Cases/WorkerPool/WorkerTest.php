310b552c89d5802ef614eaa181f182f2e7232b12
alwaysLinger <43213269+alwaysLinger@users.noreply.github.com>
2025-06-17 12:14:50 +0800

    fix:tests and ConfigProvider namespace

--- tests/Cases/WorkerPool/WorkerTest.php
+++ tests/Cases/WorkerPool/WorkerTest.php
@@ -10,7 +10,7 @@ declare(strict_types=1);
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
 
-namespace Hyperf\Incubator\Cases\WorkerPool;
+namespace HyperfTest\Incubator\Cases\WorkerPool;
 
 use Hyperf\Incubator\WorkerPool\Exception\RuntimeException;
 use Hyperf\Incubator\WorkerPool\Pool\Contracts\WithNodeInterface;
