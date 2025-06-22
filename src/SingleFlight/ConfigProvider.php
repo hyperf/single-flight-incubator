cdaf411cc7608acba6191c7e3f23bed5d61f4cbe
alwaysLinger <43213269+alwaysLinger@users.noreply.github.com>
2025-06-22 23:59:25 +0800

    fix:add single-flight aspects config

--- src/SingleFlight/ConfigProvider.php
+++ src/SingleFlight/ConfigProvider.php
@@ -12,6 +12,8 @@ declare(strict_types=1);
 
 namespace Hyperf\Incubator\SingleFlight;
 
+use Hyperf\Incubator\SingleFlight\Aspect\SingleFlightAspect;
+
 class ConfigProvider
 {
     public function __invoke(): array
@@ -23,6 +25,9 @@ class ConfigProvider
                         __DIR__,
                     ],
                 ],
+            ],
+            'aspects' => [
+                SingleFlightAspect::class,
             ],
         ];
     }
